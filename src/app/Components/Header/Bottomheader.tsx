52485d1df36dffc0939d0e9697050adbb477b88e
KhulaJaved <khulajaved1995@gmail.com>
2024-12-08 21:31:22 +0500

    updated

--- src/app/Components/Header/Bottomheader.tsx
+++ src/app/Components/Header/Bottomheader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Bottomheader = () => {
   return (
     <div>
-        bottom header
+        
     </div>
   )
 }
--- src/app/Components/Header/Middleheader.tsx
+++ src/app/Components/Header/Middleheader.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Image from "next/image"
 
 const Middleheader = () => {
   return (
-    <div>Middleheader</div>
+    <div>
+      <div className='flex flex-row justify-between items-center px-5 sm:px-10 lg:px-[300px] py-5 gap-5 lg:gap-[302px] w-full max-w-[1920px] h-[84px] bg-[#F0F2F3]'>
+        <div className='flex flex-row items-center gap-2 mx-auto w-[166px] h-[40px] sm:w-[140px] sm:h-[35px] lg:w-[166px] lg:h-[40px]'>
+        <div className='w-[40px] h-[40px] sm:w-[30px] sm:h-[30px] lg:w-[40px] lg:h-[40px]'>
+        <Image src={'/Logo Icon.png'} alt='image' width={26.67} height={23.33} />
+        </div>
+        
+        <div>Comforty</div>
+        
+        </div>
+
+      </div>
+  
+    </div>
+    
   )
 }
 
 export default Middleheader
+
+
+
+
+
+
+
